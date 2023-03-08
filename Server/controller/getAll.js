57496bf07db03718e2911afcbb98427070103384
Sang rongyong <s6401012620242@email.kmutnb.ac.th>
2023-03-08 22:50:32 +0700

    test query

--- Server/controller/getAll.js
+++ Server/controller/getAll.js
@@ -3,14 +3,14 @@ const sqlite3 = require('sqlite3').verbose();
 // open the database
 let db = new sqlite3.Database('./data.sqlite');
 
-let query = 'SELECT * FROM Group Members';
+let query = 'SELECT * FROM Members';
 
 db.all(query, [], (err, rows) => {
   if (err) {
     throw err;
   }
   rows.forEach((row) => {
-    console.log(row.name);
+    console.log(row);  // <------- return all in row for each member
   });
 });
 
