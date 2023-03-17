2ad94ed3a22f4cd05c3b8e9b81849f708e936b61
ThanawatTP <thanawat.tp@outlook.com>
2023-03-17 16:00:56 +0700

    ThailandTopo change color shade,splite total_value with max weeknum condition for each year, sum value every total_value all year

--- ChartManager.py
+++ ChartManager.py
@@ -20,11 +20,11 @@ class ChartManager():
         self.obj_columns = self.df.select_dtypes(include=['object']).columns.to_list()
         self.int_columns = self.df.select_dtypes(include=['int']).columns.to_list()
     
-    def ColorSchema(self,Measurement,Color:list):
+    def ColorSchema(self,dataframe,Measurement,Color:list):
         color = alt.Color(Measurement,
                           type= "quantitative",
                           scale = alt.Scale(
-                            domain = [0,self.df[Measurement].max()],
+                            domain = [0,dataframe[Measurement].mean(),dataframe[Measurement].max()],
                             range = Color,
                             type = "linear")
                         )
@@ -36,16 +36,29 @@ class ChartManager():
             t = "quantitative" if col in self.int_columns else "nominal"
             l.append(alt.Tooltip(col, type= t))
         return l
+
+    def SumDuplicateValue(self):
+        l = []
+        for y in list(set(self.df['year'])):
+            maxweeknum = self.df.loc[self.df['year'] == y]['weeknum'].max()
+            l.append(self.df.loc[(self.df['weeknum'] == maxweeknum) & (self.df['year'] == y)])
+        df = pd.concat(l, ignore_index=True)
+        group = df.groupby('province').transform('sum')
+        df['total_case'] = group['total_case']
+        df['total_death'] = group['total_death']
+        df = df.drop_duplicates(subset=['province'])
+        return df
     
     def ThailandTopoChart(self,Width,Height):
         chart = alt.Chart(self.ThailandProvincesTopo).mark_geoshape().encode(
-            color = self.ColorSchema('total_case',['white','darkred']),
-            tooltip = self.Tooltip(['properties.NAME_1','total_case'])
+            color = self.ColorSchema(self.SumDuplicateValue(),'total_case',['white','#E34234','#640000']),
+            tooltip = self.Tooltip(['properties.NAME_1','total_case','total_death'])
         ).transform_lookup(
             lookup='properties.NAME_1',
-            from_ = alt.LookupData(self.df,'province',['total_case','total_death'])
+            from_ = alt.LookupData(self.SumDuplicateValue(),'province',['total_case','total_death'])
         ).properties(
             width=Width,
             height=Height
         )
+        chart.save('ChartJSON/ThailandTopoChart.json')
         return chart
