cb523fecea0a002b22abda62c5811136651fcc16
ThanawatTP <thanawat.tp@outlook.com>
2023-03-23 23:55:03 +0700

    independent self.df to df & dropAllNone replace false

--- ChartManager.py
+++ ChartManager.py
@@ -37,11 +37,11 @@ class ChartManager():
             l.append(alt.Tooltip(col, type= t))
         return l
 
-    def SumDuplicateValue(self):
+    def SumDuplicateValue(self,df):
         l = []
-        for y in list(set(self.df['year'])):
-            maxweeknum = self.df.loc[self.df['year'] == y]['weeknum'].max()
-            l.append(self.df.loc[(self.df['weeknum'] == maxweeknum) & (self.df['year'] == y)])
+        for y in list(set(df['year'])):
+            maxweeknum = df.loc[df['year'] == y]['weeknum'].max()
+            l.append(df.loc[(df['weeknum'] == maxweeknum) & (df['year'] == y)])
         df = pd.concat(l, ignore_index=True)
         group = df.groupby('province').transform('sum')
         df['total_case'] = group['total_case']
@@ -50,38 +50,38 @@ class ChartManager():
         return df
     
     def dropAllandNone(self,df):
-        dff = df
-        dff.drop(index=dff[dff["province"].isin(["All","None"])].index, inplace=True)
-        return dff
+        df = df.drop(index=df[df["province"].isin(["All","None"])].index)
+        return df
 
-    def SetDatetime(self):
-        
-        self.df['date'] = pd.to_datetime(
-                            self.df['year'].astype(str) + '-W' + self.df['weeknum'].astype(str) + '-0',
+    def SetDatetime(self,df):
+        df['date'] = pd.to_datetime(
+                            df['year'].astype(str) + '-W' + df['weeknum'].astype(str) + '-0',
                             format='%G-W%V-%w')
-        self.df = self.df.sort_values(by=['date']).loc[self.df["province"]=="All"].reset_index(drop=True)
+        df = df.sort_values(by=['date']).loc[df["province"]=="All"].reset_index(drop=True)
+        return df
         
-    def SumTotalCD(self):
-        self.df['allcase'] = 0
-        self.df['alldeath'] = 0
-        for index, row in self.df.iterrows():
+    def SumTotalCD(self,df):
+        df['allcase'] = 0
+        df['alldeath'] = 0
+        for index, row in df.iterrows():
             if index == 0:
-                self.df.loc[index,'allcase'] = row['total_case']
-                casenextvalue = self.df.loc[index,'allcase']
-                self.df.loc[index,'alldeath'] = row['total_death']
-                deathnextvalue = self.df.loc[index,'alldeath']
-            elif index == self.df.shape[0] -1:
-                self.df.loc[index,'allcase'] = casenextvalue + row['new_case']
-                self.df.loc[index,'alldeath'] = deathnextvalue + row['new_death']
+                df.loc[index,'allcase'] = row['total_case']
+                casenextvalue = df.loc[index,'allcase']
+                df.loc[index,'alldeath'] = row['total_death']
+                deathnextvalue = df.loc[index,'alldeath']
+            elif index == df.shape[0] -1:
+                df.loc[index,'allcase'] = casenextvalue + row['new_case']
+                df.loc[index,'alldeath'] = deathnextvalue + row['new_death']
             else:
-                self.df.loc[index,'allcase'] = casenextvalue + row['new_case']
-                casenextvalue = self.df.loc[index,'allcase']
-                self.df.loc[index,'alldeath'] = deathnextvalue + row['new_death']
-                deathnextvalue = self.df.loc[index,'alldeath']
+                df.loc[index,'allcase'] = casenextvalue + row['new_case']
+                casenextvalue = df.loc[index,'allcase']
+                df.loc[index,'alldeath'] = deathnextvalue + row['new_death']
+                deathnextvalue = df.loc[index,'alldeath']
+        return df
 
     
     def ThailandTopoChart(self,Width,Height):
-        df = self.SumDuplicateValue()
+        df = self.SumDuplicateValue(self.df)
         df = self.dropAllandNone(df)
         self.Chart = alt.Chart(self.ThailandProvincesTopo).mark_geoshape().encode(
             color = self.ColorSchema(df,'total_case',['white','#E34234','#640000']),
@@ -112,9 +112,9 @@ class ChartManager():
         return self.Chart
 
     def LineChart(self):
-        self.SetDatetime()
-        self.SumTotalCD()
-        self.Chart = alt.Chart(self.df).mark_line(point=alt.OverlayMarkDef(filled=False, fill="white")
+        df = self.SetDatetime(self.df)
+        df = self.SumTotalCD(df)
+        self.Chart = alt.Chart(df).mark_line(point=alt.OverlayMarkDef(filled=False, fill="white")
                                 ).encode(
             x=alt.X("date",type="temporal", title= "วัน"),
             y=alt.Y(
