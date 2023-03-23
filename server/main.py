f2c0a92900fe73ec355bed9b0bf436ef8bee2b4e
Nattavee Narischat <nattavee123vee@gmail.com>
2023-03-24 01:19:02 +0700

    Update Cors server

--- server/main.py
+++ server/main.py
@@ -6,6 +6,13 @@ from pydantic import BaseModel
 import pandas as pd
 import altair as alt
 #import altair_viewer
+from fastapi.middleware.cors import CORSMiddleware
+
+
+
+app = FastAPI()
+
+#import altair_viewer
 
 ###วิธีการใช้งาน
 ###pip install uvicorn
@@ -16,7 +23,21 @@ import altair as alt
 #http://127.0.0.1:8000/docs สามารถดู API ทั้งหมดได้
 
 
-app = FastAPI()
+
+
+
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 alt.data_transformers.disable_max_rows()
 # altair_viewer._global_viewer._use_bundled_js = False
@@ -27,16 +48,19 @@ class ChartManager():
     def __init__(self):
         self.df = None
         self.Chart = None
+        #Thailand Map Chart
         self.ThailandProvincesTopo = alt.topo_feature('https://raw.githubusercontent.com/cvibhagool/thailand-map/master/thailand-provinces.topojson', 'province')
-        self.obj_columns = []
-        self.int_columns = []
+        self.obj_columns = []   #columns of object value for nomial type
+        self.int_columns = []   #columns of integer value for quantitative type
     
     def setDataframe(self,dataframe):
         self.df = dataframe
+        #select columns name each type
         self.obj_columns = self.df.select_dtypes(include=['object']).columns.to_list()
         self.int_columns = self.df.select_dtypes(include=['int']).columns.to_list()
     
     def ColorSchema(self,dataframe,Measurement,Color:list):
+        #set color shades from max value
         color = alt.Color(Measurement,
                           type= "quantitative",
                           scale = alt.Scale(
@@ -47,6 +71,7 @@ class ChartManager():
         return color
     
     def Tooltip(self,ColumnNames:list):
+        #select tooltip type each column names
         l = []
         for col in ColumnNames:
             t = "quantitative" if col in self.int_columns else "nominal"
@@ -56,20 +81,25 @@ class ChartManager():
     def SumDuplicateValue(self,df):
         l = []
         for y in list(set(df['year'])):
-            maxweeknum = df.loc[df['year'] == y]['weeknum'].max()
+            maxweeknum = df.loc[df['year'] == y]['weeknum'].max()                       #find last week in this year
+            #get dataframe with rows at last week using total_case each year
             l.append(df.loc[(df['weeknum'] == maxweeknum) & (df['year'] == y)])
         df = pd.concat(l, ignore_index=True)
+        #sum total_case and deatch each province
         group = df.groupby('province').transform('sum')
+        #move to dataframe
         df['total_case'] = group['total_case']
         df['total_death'] = group['total_death']
         df = df.drop_duplicates(subset=['province'])
         return df
     
     def dropAllandNone(self,df):
+        #drop All and None rows
         df = df.drop(index=df[df["province"].isin(["All","None"])].index)
         return df
 
     def SetDatetime(self,df):
+        #Change value in date column to datetime type 
         df['date'] = pd.to_datetime(
                             df['year'].astype(str) + '-W' + df['weeknum'].astype(str) + '-0',
                             format='%G-W%V-%w')
@@ -77,6 +107,7 @@ class ChartManager():
         return df
         
     def SumTotalCD(self,df):
+        #sum total_case and total_case of each year for all year to allcase and alldeath
         df['allcase'] = 0
         df['alldeath'] = 0
         for index, row in df.iterrows():
@@ -99,6 +130,7 @@ class ChartManager():
     def ThailandTopoChart(self,Width,Height):
         df = self.SumDuplicateValue(self.df)
         df = self.dropAllandNone(df)
+
         self.Chart = alt.Chart(self.ThailandProvincesTopo).mark_geoshape().encode(
             color = self.ColorSchema(df,'total_case',['white','#E34234','#640000']),
             tooltip = self.Tooltip(['properties.NAME_1','total_case','total_death'])
@@ -109,12 +141,14 @@ class ChartManager():
             width=Width,
             height=Height
         )
+
         #self.Chart.save('ChartJSON/ThailandTopoChart.json')
         return self.Chart.to_json()
     
     def BarChart(self):
         df = self.df
         df = self.dropAllandNone(df)
+
         self.Chart = alt.Chart(df).mark_bar(clip=True).encode(
             x = alt.X("province",type = "nominal", title= "จังหวัด"),
             y = alt.Y("total_case", 
@@ -124,27 +158,32 @@ class ChartManager():
             tooltip = ["province","total_case","total_death"]
         ).facet( column = "region"
         ).resolve_scale(x = 'independent',y = 'independent')
+
         #self.Chart.save('ChartJSON/BarChart.json')
         return self.Chart.to_json()
 
     def LineChart(self):
         df = self.SetDatetime(self.df)
         df = self.SumTotalCD(df)
-        self.Chart = alt.Chart(df).mark_line(point=alt.OverlayMarkDef(filled=False, fill="white")
-                                ).encode(
-            x=alt.X("date",type="temporal", title= "วัน"),
-            y=alt.Y(
-                alt.repeat("layer"), aggregate="mean",title="ผู้ติดเชื้อสะสมทั้งประเทศ"),
-            tooltip = ['date:T','allcase:Q','alldeath:Q'],
-            color=alt.datum(alt.repeat("layer")),
+        
+        self.Chart = alt.Chart(df).mark_line(
+            point=alt.OverlayMarkDef(
+                filled=False, fill="white")
+            ).encode(
+                x=alt.X("date",type="temporal", title= "วัน"),
+                y=alt.Y(
+                    alt.repeat("layer"), aggregate="mean",title="ผู้ติดเชื้อสะสมทั้งประเทศ"),
+                tooltip = ['date:T','allcase:Q','alldeath:Q'],
+                color=alt.datum(alt.repeat("layer")),
         ).repeat(layer=["allcase", "alldeath"])
+
         #self.Chart.save('ChartJSON/LineChart.json')
         return self.Chart.to_json()
 
 
 
 def plot_bar(year,country):
-    conn = sqlite3.connect('../Server/Covid.db')
+    conn = sqlite3.connect('./Covid.db')
     if year == 'all':
         query = "SELECT *,SUM([total_case]) AS [total_cases] FROM alldata_province_eng WHERE ([year] = 2021 OR [year] = 2023) OR ([year] = 2022 AND [weeknum] = 52) GROUP BY [province]"
         df = pd.read_sql(query , conn)
@@ -177,7 +216,7 @@ def plot_bar(year,country):
     return obj.BarChart()
 
 def plot_line(year,country):
-    conn = sqlite3.connect('../Server/Covid.db')
+    conn = sqlite3.connect('./Covid.db')
     if year == 'all':
         query = "SELECT *,SUM([total_case]) AS [total_cases] FROM alldata_province_eng WHERE ([year] = 2021 OR [year] = 2023) OR ([year] = 2022 AND [weeknum] = 52) GROUP BY [province]"
         df = pd.read_sql(query , conn)
@@ -211,7 +250,7 @@ def plot_line(year,country):
     return obj.LineChart()
 
 def plot_TH(year,country):
-    conn = sqlite3.connect('../Server/Covid.db')
+    conn = sqlite3.connect('./Covid.db')
     if year == 'all':
         query = "SELECT *,SUM([total_case]) AS [total_cases] FROM alldata_province_eng WHERE ([year] = 2021 OR [year] = 2023) OR ([year] = 2022 AND [weeknum] = 52) GROUP BY [province]"
         df = pd.read_sql(query , conn)
@@ -245,7 +284,7 @@ def plot_TH(year,country):
     return obj.ThailandTopoChart(500,600)
 
 def plot_country(year,country):
-    conn = sqlite3.connect('../Server/Covid.db')
+    conn = sqlite3.connect('./Covid.db')
     if year == 'all':
         query = "SELECT *,SUM([total_case]) AS [total_cases] FROM alldata_province_eng WHERE ([year] = 2021 OR [year] = 2023) OR ([year] = 2022 AND [weeknum] = 52) GROUP BY [province]"
         df = pd.read_sql(query , conn)
@@ -293,7 +332,7 @@ async def read_Country_graph(request: input):
             'year': request.year,
             'area': request.area
         }
-        if data['year'] == [2022,2023]:
+        if data['year'] == [2021,2022,2023]:
                 Result_plot = plot_country('all',data['area'])
         else:
             Result_plot = plot_country(data['year'][0],data['area'])
@@ -311,7 +350,7 @@ async def plot_barChart(request: input):
             'year': request.year,
             'area': request.area
         }
-        if data['year'] == [2022,2023]:
+        if data['year'] == [2021,2022,2023]:
                 Result_plot_bar = plot_bar('all',data['area'])
         else:
             Result_plot_bar = plot_bar(data['year'][0],data['area'])
@@ -329,7 +368,7 @@ async def plot_lineChart(request: input):
             'year': request.year,
             'area': request.area
         }
-        if data['year'] == [2022,2023]:
+        if data['year'] == [2021,2022,2023]:
                 Result_plot_line = plot_line('all',data['area'])
         else:
             Result_plot_line = plot_line(data['year'][0],data['area'])
